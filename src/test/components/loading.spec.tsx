0005bc8c7e7d0b5a6d8711ea84972aa05c13e1b2
RanielliMontagna <raniellimontagna@hotmail.com>
2023-04-26 13:43:14 -0300

    test: :white_check_mark: adiciona teste no componente de loading

---
+++ src/test/components/loading.spec.tsx
@@ -0,0 +1,24 @@
+import { render } from '@testing-library/react'
+
+import { QuantunProvider } from '@quantun/core'
+import { Loading } from 'components/loading'
+
+describe('@components/loading', () => {
+  it('should render without crashing', () => {
+    const { container } = render(<Loading />)
+    expect(container).toBeTruthy()
+  })
+
+  it('should render dark mode without crashing', () => {
+    const { container } = render(
+      <QuantunProvider
+        theme={{
+          colorScheme: 'dark',
+        }}
+      >
+        <Loading />
+      </QuantunProvider>,
+    )
+    expect(container).toBeTruthy()
+  })
+})
