d662d70d784afcc81b319ab72cc5d4f80987655a
RanielliMontagna <raniellimontagna@hotmail.com>
2023-04-26 14:15:12 -0300

    refactor: :art: faz melhorias referentes ao pwa

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,7 @@ const pwaOptions: Partial<VitePWAOptions> = {
         src: '/pwa-512x512.png',
         sizes: '512x512',
         type: 'image/png',
+        purpose: 'any maskable',
       },
     ],
   },
