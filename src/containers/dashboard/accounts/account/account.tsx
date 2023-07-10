4e3a8588b4838e881dd183f686600d49f2befa35
Ranielli Montagna <raniellimontagna@hotmail.com>
2023-07-10 16:41:36 -0300

    refactor: :recycle: faz pequeno ajuste na bottombar e estilização de como é mostrada as contas na dashboard

--- src/containers/dashboard/accounts/account/account.tsx
+++ src/containers/dashboard/accounts/account/account.tsx
@@ -1,4 +1,4 @@
-import { Flex, Title, Text, Image } from '@mantine/core'
+import { Flex, Title, Text, Image, Tooltip } from '@mantine/core'
 
 import { ResponseAccount } from 'api/accounts/accounts.types'
 import { institutionLogoMap } from 'containers/accounts/accounts.static'
@@ -9,9 +9,16 @@ interface IAccountProps extends ResponseAccount {}
 export function Account({ name, institution, balance }: IAccountProps) {
   return (
     <Flex justify="space-between">
-      <Flex align="center" gap={4}>
+      <Flex align="center" gap={4} style={{ width: 'calc(100% - 100px)' }}>
         <Image src={institutionLogoMap[institution]} width={18} height={18} />
-        <Text size="md">{name}</Text>
+        <Tooltip label={name} withArrow openDelay={1000}>
+          <Text
+            size="md"
+            style={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}
+          >
+            {name}
+          </Text>
+        </Tooltip>
       </Flex>
       <Title order={6}>{currencyFormat(balance)}</Title>
     </Flex>
