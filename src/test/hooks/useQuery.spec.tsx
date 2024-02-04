803fcbc97070bd7a0e17f43d8cd45542cceace05
Ranielli Montagna <raniellimontagna@hotmail.com>
2024-02-04 15:52:38 -0300

    test: :white_check_mark: ajusta teste do useQuery

--- src/test/hooks/useQuery.spec.tsx
+++ src/test/hooks/useQuery.spec.tsx
@@ -1,12 +1,21 @@
-import { renderHook } from '@testing-library/react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { renderHook, waitFor } from '@testing-library/react'
 import { useQuery } from 'hooks/useQuery'
 
 describe('@hooks/useQuery', () => {
   it('should return the correct response', async () => {
-    const { result } = renderHook(() => useQuery({ queryKey: 'key' }))
-    expect(result.current.data).toBeUndefined()
+    const queryClient = new QueryClient()
 
-    result.current.refetch()
-    expect(result.current.data).toBeUndefined()
+    const wrapper = ({ children }: { children: React.ReactNode }) => (
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    )
+
+    waitFor(() => {
+      const { result } = renderHook(() => useQuery({ queryKey: 'key' }), { wrapper })
+      expect(result.current.data).toBeUndefined()
+
+      result.current.refetch()
+      expect(result.current.data).toBeUndefined()
+    })
   })
 })
